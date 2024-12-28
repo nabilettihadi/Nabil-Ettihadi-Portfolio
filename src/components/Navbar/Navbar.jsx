bf6c8fe5e643cba8ce052a48b1ec416ea6a72a3a
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2024-12-28 13:11:47 +0100

    Mise à jour du lien Portfolio et des assets

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="/">
+      <a className={styles.title} href="https://nabilettihadi.github.io/Nabil-Ettihadi-Portfolio/">
         Portfolio
       </a>
       <div className={styles.menu}>
