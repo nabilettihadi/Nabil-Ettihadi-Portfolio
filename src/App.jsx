625d1af90c012ee18831f1988fed705e1a8cbc81
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2024-12-28 12:00:36 +0100

    Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import styles from "./App.module.css";
-import { About } from "./components/About/About";
-import { Contact } from "./components/Contact/Contact";
-import { Experience } from "./components/Experience/Experience";
-import { Hero } from "./components/Hero/Hero";
-import { Navbar } from "./components/Navbar/Navbar";
-import { Projects } from "./components/Projects/Projects";
+import styles from "@/App.module.css";
+import { About } from "@components/About/About";
+import { Contact } from "@components/Contact/Contact";
+import { Experience } from "@components/Experience/Experience";
+import { Hero } from "@components/Hero/Hero";
+import { Navbar } from "@components/Navbar/Navbar";
+import { Projects } from "@components/Projects/Projects";
 
 function App() {
   return (
