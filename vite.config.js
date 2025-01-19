28d0c9411b46d943af69096d31c6c053ff16ae47
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2025-01-19 19:38:32 +0100

    fix: update vite config with full GitHub Pages URL

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Nabil-Ettihadi-Portfolio/',
+  base: 'https://nabilettihadi.github.io/Nabil-Ettihadi-Portfolio/',
   assetsInclude: ['**/*.pdf'],
   build: {
     rollupOptions: {
