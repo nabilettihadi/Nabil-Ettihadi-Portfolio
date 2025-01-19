2b25758bd5376c095ca0d174936ce3c5048dfca2
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2025-01-19 16:59:16 +0100

    Deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Nabil-Ettihadi-Portfolio/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  },
-  server: {
-    port: 3000,
-    open: true
-  }
+  base: '/Nabil-Ettihadi-Portfolio/'
 })
