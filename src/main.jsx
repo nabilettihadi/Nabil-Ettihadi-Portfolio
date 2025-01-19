5f6f6908a58d82311f24801cab0fc35a8fc502ff
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2025-01-19 19:12:14 +0100

    feat: amélioration des performances et de l'expérience utilisateur

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "./i18n";
+import { initGA, logPageView } from './utils/analytics';
 
 import "@fontsource/outfit";
 import "@fontsource/roboto";
 
+initGA();
+logPageView();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
