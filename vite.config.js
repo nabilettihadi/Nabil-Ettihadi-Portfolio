d7580d08725ab3cf83f43b315c8b907227b9f1de
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2025-01-19 16:40:44 +0100

    chore: clean project and simplify deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Nabil-Ettihadi-Portfolio/',
+  base: '/',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      }
-    }
+    outDir: 'dist'
   }
 })
